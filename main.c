1cb2a456f9e382eae3aa500b7c5d26e3b769573e
Lucas <lucas.costa.123@ufrn.edu.br>
2025-08-15 00:05:55 -0300

    feat: Adicionando a tela Sobre

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <windows.h>
+
+void telaSobre(void);
 
 int main(void){
-    printf("Projeto SIG-Circus\n");
+
+    SetConsoleOutputCP(65001); // UTF-8
+    SetConsoleCP(65001);
+
+    telaSobre();
     return 0;
+}
+
+void telaSobre(void){
+    printf("\n");
+    printf("==============================================================================\n");
+    printf("||                                                                          ||\n");
+    printf("||             Universidade Federal do Rio Grande do Norte                  ||\n");
+    printf("||                 Centro de Ensino Superior do Seridó                      ||\n");
+    printf("||               Departamento de Computação e Tecnologia                    ||\n");
+    printf("||                  Disciplina DCT1106 -- Programação                       ||\n");
+    printf("||                         Projeto SIG-Circus                               ||\n");
+    printf("||             Developed by @lucascsantos07 -- since Aug, 2025              ||\n");
+    printf("||                                                                          ||\n");
+    printf("==============================================================================\n");
+    printf("||                                                                          ||\n");
+    printf("||               ~ ~ ~ Sistema de Gestão Para Um Circo ~ ~ ~                ||\n");
+    printf("||                                                                          ||\n");
+    printf("||  O SIG-Circus é um sistema desenvolvido para facilitar a gestão          ||\n");
+    printf("||  completa de um circo, proporcionando controle eficiente de todas        ||\n");
+    printf("||  as operações essenciais. Com uma interface intuitiva, o sistema         ||\n");
+    printf("||  permite organizar clientes, funcionários, vendas de ingressos,          ||\n");
+    printf("||  relatórios e toda a programação de eventos de forma prática e segura.   ||\n");
+    printf("||  O objetivo do SIG-Circus é otimizar os processos internos, tornando     ||\n");
+    printf("||  a administração do circo mais ágil, organizada e eficiente, oferecendo  ||\n");
+    printf("||  suporte tanto para a equipe quanto para o público.                      ||\n");
+    printf("||                                                                          ||\n");
+    printf("==============================================================================\n");
+    printf("\n");
 }
