f5fb2ba9d0f2c0f0c932855e19cfb12bab7deb9b
Icaro-Nonato <icaro.freitas.113@ufrn.edu.br>
2025-10-20 21:40:43 +0000

    Assinatura de Ingressos e Arquivos .dat

--- Ingressos/ingressos.h
+++ Ingressos/ingressos.h
@@ -1,10 +1,11 @@
 // Assinatura das funcoes do modulo vendas de ingressos
 
+
 typedef struct ingressos Ingressos;
 
 struct ingressos {
     char cpfCliente[20];
-    int quantidadeIngressos, escolha, idEspetaculo, id;
+    int quantidadeIngressos, idEspetaculo, id, status;
     float valorTotal;
     char data[12], cpfResponsavel[20], cidade[50], horario[7], dataLida[12], cpfLido[20];
     int capacidadeMax;
@@ -17,3 +18,10 @@ void telaCadastroVendaIngresso(void);
 void consultarVendaIngresso(void);
 void reembolsarVendaIngresso(void);
 void exibirModuloVendasIngressos(void);
+
+Ingressos* ColetarDadosIngressos(void);
+void ExibirIngresso(Ingressos* ingresso);
+void ConfirmarCadastroIngresso(Ingressos* ingresso);
+void SalvarIngresso(FILE* fp_ingressos, Ingressos* ingresso);
+void ExibirTodosAgendamentos(void);
+int IngressoMaiorID(void);
